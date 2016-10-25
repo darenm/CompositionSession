b4551c79e24e48853495c00f3841023032714660
Daren May <daren.may@gmail.com>
2016-10-25 11:56:31 -0600

    Built rich sample

---
+++ MediaSample-Complete/Converters/PosterConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+using MediaSample.Services;
+
+namespace MediaSample.Converters
+{
+    public class PosterConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value as Poster;
+        }
+
+        #endregion
+    }
+}
---
+++ MediaSample-Complete/Services/PosterService.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace MediaSample.Services
+{
+    public class PosterService
+    {
+        public async Task<ObservableCollection<Poster>> GetPostersAsync()
+        {
+            await Task.Delay(500);
+            var posters = new ObservableCollection<Poster>
+            {
+                new Poster
+                {
+                    Title = "A Clockwork Orange",
+                    ImagePath = "ms-appx:///Assets/1.jpg",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                },
+                new Poster
+                {
+                    Title = "Inception",
+                    ImagePath = "ms-appx:///Assets/2.jpg",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                },
+                new Poster
+                {
+                    Title = "Back to the Future",
+                    ImagePath = "ms-appx:///Assets/3.jpg",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                },
+                new Poster
+                {
+                    Title = "Pulp Fiction",
+                    ImagePath = "ms-appx:///Assets/4.jpg",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                },
+                new Poster
+                {
+                    Title = "Blade Runner",
+                    ImagePath = "ms-appx:///Assets/5.jpg",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                },
+                new Poster
+                {
+                    Title = "American Psycho",
+                    ImagePath = "ms-appx:///Assets/6.jpg",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                },
+                new Poster
+                {
+                    Title = "7 Samurai",
+                    ImagePath = "ms-appx:///Assets/7.jpg",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                },
+                new Poster
+                {
+                    Title = "The Goonies",
+                    ImagePath = "ms-appx:///Assets/8.jpg",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                },
+                new Poster
+                {
+                    Title = "Psycho",
+                    ImagePath = "ms-appx:///Assets/9.jpg",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                },
+                new Poster
+                {
+                    Title = "The Prestige",
+                    ImagePath = "ms-appx:///Assets/10.jpg",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                },
+                new Poster
+                {
+                    Title = "Friday the 13th",
+                    ImagePath = "ms-appx:///Assets/11.jpg",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                },
+                new Poster
+                {
+                    Title = "Leon - The Professional",
+                    ImagePath = "ms-appx:///Assets/12.jpg",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                },
+            };
+
+            return posters;
+        }
+    }
+
+    public class Poster
+    {
+        public string ImagePath { get; set; }
+
+        public BitmapImage ImageSource => new BitmapImage(new Uri(ImagePath));
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
